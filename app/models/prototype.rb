8b79374a44530963a21e985b3cf61f4b84a8f81a
Christian M Londono <christianlondono91@gmail.com>
2013-03-20 02:57:58 -0400

    fixed prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ActiveRecord::Base
 	attr_accessible :material, :design, :tolerance, :volume, :comments
 
 	validates :design, :presence => true
-	validates :volume, :presense => true
+	validates :volume, :presence => true
 	
 
 
