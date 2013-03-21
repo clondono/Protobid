31060fae83bbfae57f18a5a5455d66b2f1276f6a
Christian M Londono <christianlondono91@gmail.com>
2013-03-21 02:57:13 -0400

    tried to fix attr. problem\

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,5 @@
 class Prototype < ActiveRecord::Base
+	attr_accessible :attachment, :material, :tolerance, :volume, :units, :comment, :attachment_file_name
 	validates :volume, :numericality => { :greater_than_or_equal_to => 0.0}
 
 #paperclip
